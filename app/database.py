8c1587fef6f485457b68eec4fafc6a32fd785dc9
BELOUAHAR Sofia <sooyaa254@gmail.com>
2026-03-30 15:01:14 +0100

    [03/30/2026 15:01:14] feat/models : implement all ORM models

--- app/database.py
+++ app/database.py
@@ -49,4 +49,7 @@ else:
 # In short, you lose all the conveniences of the ORM and must manage tables and queries manually.
 Base = declarative_base()
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 5e13da6 ([03/30/2026 12:07:13] feat/models : implement all ORM models)
