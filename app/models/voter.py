8d22df415acc82d178120aa98b90a9c994bfe3dc
BELOUAHAR Sofia <sooyaa254@gmail.com>
2026-03-30 11:57:00 +0100

    [03/30/2026 11:57:00] feat/models : implement all ORM models

---
+++ app/models/voter.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+
+class Voter(Base):
+    __tablename__ = "voters"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, unique=True)
