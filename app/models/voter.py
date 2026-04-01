78491ace6aee7a8203108a1abe291292f8f29997
walid-hammouti <w.walidcontacts@gmail.com>
2026-04-01 13:38:40 +0100

    Reapply "Merge pull request #5 from Crypto-Project-ENSTA/feat/models"
    This reverts commit 1e3df0a145b68df7155951818709d4bc93d01352.

--- app/models/voter.py
+++ app/models/voter.py
@@ -1,12 +1,11 @@
-# ─────────────────────────────────────────────
-# @sophia
-# All models must inherit from Base (imported from app.database in the file app/database.py)
-#
-# Start every model like this:
-#
-#   from app.database import Base
-#
-#   class YourModel(Base): <-- this is the orm model name 
-#       __tablename__ = "your_table" <-- this is the sql table name (same as excalidraw MCD)
-#       ...
-# ─────────────────────────────────────────────
+
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+
+class Voter(Base):
+    __tablename__ = "voters"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, nullable=False, unique=True)
+    
