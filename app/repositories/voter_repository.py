9f58a0a9aeaf0c8404ce90681d2d7152b8bc1d46
walid-hammouti <w.walidcontacts@gmail.com>
2026-04-07 23:37:29 +0100

    [Tue Apr  7 11:37:29 PM CET 2026] feat/voting-config-model-and-repo : implement voting configuration repository

--- app/repositories/voter_repository.py
+++ app/repositories/voter_repository.py
@@ -12,12 +12,12 @@ def create_voter(db :Session , voter =VoterSchema ):
     db.refresh(db_voter)
     return db_voter
 
-# this function is responsible for checking if the voters pool is full or not 
-def check_voter_limit(db: Session):
-    nbrs_of_lines= (db.query(VoterModel).count())
-    if ((nbrs_of_lines) == VotingSystemConfig.num_voters):
-        return True
-    return False
+# # this function is responsible for checking if the voters pool is full or not 
+# def check_voter_limit(db: Session):
+#     nbrs_of_lines= (db.query(VoterModel).count())
+#     if ((nbrs_of_lines) == VotingSystemConfig.num_voters):
+#         return True
+#     return False
 
 def save_voter_credentials(n1:str, n2: str , db : Session):
     hashed_n2 = hash_n2(n2)
