8ef79bffe48d68d135228552ee77a48a46325a1a
walid-hammouti <w.walidcontacts@gmail.com>
2026-04-07 23:55:43 +0100

    [Tue Apr  7 11:55:43 PM CET 2026] feat/voting-config-model-and-repo :add choices column to voting configuration model and migration script

--- app/models/voting_system_config_model.py
+++ app/models/voting_system_config_model.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Boolean, Integer, String
 from app.database import Base
+from sqlalchemy.types import JSON
 
 class VotingConfigModel(Base):
     __tablename__ = "voting_config"
@@ -8,3 +9,4 @@ class VotingConfigModel(Base):
     emails_sent = Column(Boolean, default=False)
     num_voters = Column(Integer, default=5)
     vote_theme = Column(String, nullable=True)
+    choices = Column(JSON, nullable=True)
