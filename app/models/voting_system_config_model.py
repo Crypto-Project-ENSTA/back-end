24c0545d5d4da811cb28e9779e5e173d96791ef6
walid-hammouti <w.walidcontacts@gmail.com>
2026-04-07 23:19:38 +0100

    [Tue Apr  7 11:19:38 PM CET 2026] feat/voting-config-model-and-repo : add the model

---
+++ app/models/voting_system_config_model.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Boolean, Integer, String
+from app.database import Base
+
+class VotingConfigModel(Base):
+    __tablename__ = "voting_config"
+
+    id = Column(Integer, primary_key=True)
+    emails_sent = Column(Boolean, default=False)
+    num_voters = Column(Integer, default=5)
+    vote_theme = Column(String, nullable=True)
