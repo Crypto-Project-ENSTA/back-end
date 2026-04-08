91bdfde89d0ba35cdd9a11e00a462e926be43976
walid-hammouti <w.walidcontacts@gmail.com>
2026-04-08 22:21:56 +0100

    [Wed Apr  8 10:21:56 PM CET 2026] feat/n1-validation : add commissioner service with N1 validation

---
+++ app/services/commissioner_service.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Session
+from app.repositories import credentials_repository
+
+class CommissionerService: 
+    def __init__(self, db: Session):
+        self.db = db
+    
+    
+    def is_n1_exist(self, voter_n1:str)->bool:
+        return credentials_repository.is_n1_exist(self.db,voter_n1)
