df9d22a93fe4e3f9a90a035f8f15dd3615ba6ed0
walid-hammouti <w.walidcontacts@gmail.com>
2026-04-08 23:35:40 +0100

    [Wed Apr  8 11:35:40 PM CET 2026] feat/n1-validation : decouple commissioner service using dependency injection

--- app/dependencies.py
+++ app/dependencies.py
@@ -3,8 +3,16 @@ from fastapi import Depends
 from sqlalchemy.orm import Session               
 from app.database import get_db
 from app.services.administrator_service import AdministratorService
+from app.services.commissioner_service import CommissionerService
 
 
 
-def get_administrator_service(db: Session = Depends(get_db)) -> AdministratorService:
-    return AdministratorService(db)
+def get_commissioner_service(db: Session = Depends(get_db)) -> CommissionerService:
+    return CommissionerService(db)
+
+
+def get_administrator_service(
+    db: Session = Depends(get_db),
+    commissioner: CommissionerService = Depends(get_commissioner_service)
+) -> AdministratorService:
+    return AdministratorService(db, commissioner)
--- app/services/administrator_service.py
+++ app/services/administrator_service.py
@@ -4,9 +4,9 @@ from app.services.commissioner_service import CommissionerService
 
 class AdministratorService:
     
-    def __init__(self, db: Session):
+    def __init__(self, db: Session,commissioner : CommissionerService):
         self.db = db                                  
-        self.commissioner = CommissionerService(db)   
+        self.commissioner = commissioner  
     
     def request_commissioner_n1_exist(self,voter_n1: str):
         return self.commissioner.is_n1_exist(voter_n1)
