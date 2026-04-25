21bcbe601847a4c933cc0a268b95292e89d01d48
walid-hammouti <w.walidcontacts@gmail.com>
2026-04-25 10:21:43 +0100

    [Sat Apr 25 10:21:43 AM CET 2026] feat/anonymizer-vote-reception : add the N1 to the server session so it can be retrived later when the anonymizer needs it

--- app/routers/voters_router.py
+++ app/routers/voters_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends,HTTPException
+from fastapi import APIRouter,Depends,HTTPException, Request
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from app.database import get_db
@@ -33,10 +33,13 @@ def voter_register(voter : Voter, db:Session = Depends(get_db) ):
     
     
 @router.post('/check_n1')
-def check_n1(voter_n1 : N1Request,service: AdministratorService = Depends(get_administrator_service)):
+def check_n1(request : Request,voter_n1 : N1Request,service: AdministratorService = Depends(get_administrator_service)):
     try:
         result = service.request_commissioner_n1_exist(voter_n1.n1)
         if result:
+            # Store the verified N1 code in the session so it can be retrieved
+            # during the voting step without the voter needing to send it again.
+            request.session["n1"] = voter_n1.n1
             return {"is_N1_exist": True}
         else:
             return {"is_N1_exist": False}
