350d0f434e85de3e8bb4da0165bd2aa081859281
walid-hammouti <w.walidcontacts@gmail.com>
2026-05-03 23:21:16 +0100

    [Sun May  3 11:21:16 PM CET 2026] feat/vote-counting : add decryption method for all votes and initialize administrator service

--- app/repositories/votes_repository.py
+++ app/repositories/votes_repository.py
@@ -24,5 +24,5 @@ def submit_encrypted_vote(db: Session, encrypted_vote: int) -> Vote:
     
     return new_vote
 
-def get_all_encrypted_votes(db: Session) -> list:
+def get_all_encrypted_votes(db: Session) -> list[Vote]:
     return db.query(Vote).filter(Vote.status == VoteStatus.VALID).all()
