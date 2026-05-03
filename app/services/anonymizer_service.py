cda2c0cc2e0410a4c2cb036cfe438e971420a6cd
walid-hammouti <w.walidcontacts@gmail.com>
2026-05-03 15:01:36 +0100

    [Sun May  3 03:01:36 PM CET 2026] feat/anonymizer-vote-reception : implement anonymization and submission of votes in AnonymizerService

--- app/services/anonymizer_service.py
+++ app/services/anonymizer_service.py
@@ -13,3 +13,9 @@ class AnonymizerService:
     
     def submit_encrypted_ballot(self,encrypted_vote:int):
         return submit_encrypted_vote(db=self.db,encrypted_vote=encrypted_vote)
+    
+    def anonymize_and_submit_vote(self, voter_n1: str, encrypted_vote: int) -> bool:
+        if not self.check_n1(voter_n1=voter_n1, removeN1=True):
+            return False
+        self.submit_encrypted_ballot(encrypted_vote=encrypted_vote)
+        return True
