9001814aa7eb1ac83cec3ab2899cf30626563277
walid-hammouti <w.walidcontacts@gmail.com>
2026-05-05 16:38:24 +0100

    [Tue May  5 04:38:24 PM CET 2026] feat/start-vote-endpoint :  add voting status management functions to handle voting state transition

--- app/repositories/voting_system_config_repo.py
+++ app/repositories/voting_system_config_repo.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from app.models.voting_system_config_model import VotingConfigModel
+from app.models.voting_system_config_model import VotingConfigModel, VotingStatus
 
 
 def get_voting_config(db: Session) -> VotingConfigModel:
@@ -61,3 +61,13 @@ def mark_emails_sent(db: Session):
 def emails_already_sent(db: Session) -> bool:
     config = get_voting_config(db)
     return config.emails_sent
+
+def set_voting_started(db:Session):
+    config = get_voting_config(db)
+    config.voting_status = VotingStatus.VOTE_STARTED
+    db.commit()
+    
+def set_voting_ended(db:Session):
+    config = get_voting_config(db)
+    config.voting_status = VotingStatus.VOTE_ENDED
+    db.commit()
