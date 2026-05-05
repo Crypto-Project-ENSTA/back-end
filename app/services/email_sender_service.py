0397e1f2312cf7a48f4a0dd6e14135cf414d1a72
walid-hammouti <w.walidcontacts@gmail.com>
2026-05-05 15:24:40 +0100

    [Tue May  5 03:24:40 PM CET 2026] feat/start-vote-endpoint :make the sending email pipline in a single function

--- app/services/email_sender_service.py
+++ app/services/email_sender_service.py
@@ -2,13 +2,15 @@ import traceback
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from sqlalchemy.orm import Session
 from app.database import SessionLocal
 import threading
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from app.models.voter import Voter
-from app.repositories.voter_repository import  save_voter_credentials
+from app.models.votes import Vote
+from app.repositories.voter_repository import  get_all_voters, save_voter_credentials
 from app.utils.crypto import generate_nonce
 from app.config import settings
 
@@ -19,7 +21,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from app.repositories.voting_system_config_repo import should_send_emails, mark_emails_sent,emails_already_sent
+from app.repositories.voting_system_config_repo import mark_emails_sent, should_send_emails,emails_already_sent
 
 def send_email(to_email: str):
     """Send credentials email via Gmail API with Service Account."""
@@ -351,24 +353,8 @@ def watch_voters():
                 break
 
             if should_send_emails(db):
-                voters = db.query(Voter).all()
-                print(f"Found {len(voters)} voters")
-
-                for voter in voters:
-                    try:
-                        n1, n2 = send_email(voter.email)
-                        print(f"Email sent to {voter.email}")
-                        save_voter_credentials(n1=n1, n2=n2, db=db)
-                        print(f"Credentials saved for {voter.email}")
-
-                    except Exception as voter_error:
-                        print(f"Failed for {voter.email}: {voter_error}")
-                        traceback.print_exc()
-                        continue
-
-                print("All emails sent.")
-                mark_emails_sent(db)
-                print("Marked as sent. Stopping watcher.")
+                voters = get_all_voters(db=db)
+                send_email_to_voters(db=db,voters=voters)
                 break  
 
             else:
@@ -388,3 +374,23 @@ def start_voter_watcher():
     global watcher_thread
     watcher_thread = threading.Thread(target=watch_voters, daemon=True)
     watcher_thread.start()
+    
+def send_email_to_voters(db:Session,voters:Vote):
+    print(f"Found {len(voters)} voters")
+
+    for voter in voters:
+        try:
+            n1, n2 = send_email(voter.email)
+            print(f"Email sent to {voter.email}")
+            save_voter_credentials(n1=n1, n2=n2, db=db)
+            print(f"Credentials saved for {voter.email}")
+
+        except Exception as voter_error:
+            print(f"Failed for {voter.email}: {voter_error}")
+            traceback.print_exc()
+            continue
+
+    print("All emails sent.")
+    mark_emails_sent(db)
+    print("Marked as sent. Stopping watcher.")
+
