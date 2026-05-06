c60d2da8c55d009739d8b74dc0ac742d4a529706
walid-hammouti <w.walidcontacts@gmail.com>
2026-05-06 21:25:02 +0100

    [Wed May  6 09:25:02 PM CET 2026] feat/start-vote-endpoint : fix the function

--- app/repositories/voting_system_config_repo.py
+++ app/repositories/voting_system_config_repo.py
@@ -74,5 +74,4 @@ def set_voting_ended(db:Session):
     
 def check_voting_status(db: Session):
     config = get_voting_config(db)  
-    row = config.first()
-    return row.voting_status if row else None
+    return config.voting_status 
